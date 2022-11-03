68fa7a40a2821420a8eab6d5849ae227b8d8003c
unknown <doniaskima344@gmail.com>
2022-11-03 13:36:44 +0100

    wave array

---
+++ src/Array/wave-array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class wave-array {
+    public int[] wave(int[] A) {
+        Arrays.sort(A);
+        for(int i=0;i<A.length-1;i=i+2){
+            swap(A,i,i+1);
+        }
+        return A;
+    }
+    
+    public int[] swap(int[] A, int i, int j){
+        int temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
+        return A;
+    }
+}
+
