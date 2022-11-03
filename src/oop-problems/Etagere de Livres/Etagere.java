a381d08a00cb56b00bc9b6a737dce48fb55214fb
unknown <doniaskima344@gmail.com>
2022-11-03 17:00:39 +0100

    working on Etagere class

--- src/oop-problems/Etagere de Livres/Etagere.java
+++ src/oop-problems/Etagere de Livres/Etagere.java
@@ -2,13 +2,16 @@ public class Etagere {
     Livre[] livres;
     private int nbr = 0;
 
+
+    // nombre de livres qui peut contenir l'etagere 
     public Etagere(int nbmax) {
         livres = new Livre[nbmax];
     }
-
+    // le nombre de livre q'elle contient 
     public int nbMaxLivres() {
         return livres.length;
     }
     
+
 
 }
