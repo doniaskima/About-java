5255c18f88c5a889c725f60cabb4c7c5bd3963d4
unknown <doniaskima344@gmail.com>
2022-11-06 04:57:57 +0100

    Person Clas

---
+++ src/small-projects/school management/Eleve.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Eleve {
+    protected String niveauScolaire;
+    protected int id;
+    protected Parent mer;
+    protected Parent per;
+    List<Eleve> listEleve = new ArrayList<Eleve>();
+
+    public Eleve(String nom, String prenom, int numeroTelephone) {
+        
+    }
+
+}
---
+++ src/small-projects/school management/Person.java
@@ -0,0 +1,69 @@
+import java.util.Date;
+
+public class Person {
+    protected String nom;
+    protected String prenom;
+    protected int numeroTelephone;
+    protected Adresse adresse;
+    protected java.sql.Date dateNaissance;
+    
+    public Person(String nom, String prenom, int numeroTelephone, Adresse adresse, java.sql.Date dateNaissance) {
+        super();
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroTelephone = numeroTelephone;
+        this.adresse = adresse;
+        this.dateNaissance = dateNaissance;
+    }
+
+    public Person() {
+        super();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+    public int getNumeroTelephone() {
+        return numeroTelephone;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public java.sql.Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public void setNumeroTelephone(int numeroTelephone) {
+        this.numeroTelephone = numeroTelephone;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setDateNaissance(java.sql.Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+    
+    public String toString() {
+        return "Person {" + 
+          "nom= "+nom +"\" +
+          " prenom='" + prenom + '\'' +
+          ", numeroTelephone=" + numeroTelephone +
+          ", adresse=" + adresse +
+          ", dateNaissance=" + dateNaissance +
+          '}'+"\n";
+    }
+
+
+
+}
