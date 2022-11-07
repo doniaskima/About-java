64367044adde4be413a29d29ff87ac97058c7e0d
unknown <doniaskima344@gmail.com>
2022-11-07 13:31:06 +0100

    working on it

--- src/small-projects/school management/Eleve.java
+++ src/small-projects/school management/Eleve.java
@@ -12,12 +12,54 @@ public class Eleve {
     List<Eleve> listEleve = new ArrayList<Eleve>();
 
     public Eleve(String nom, String prenom, int numeroTelephone, Adresse adresse, java.sql.Date dateNaissance,
-    String niveauScolaire, int id, Parent mer, Parent per) {
+            String niveauScolaire, int id, Parent mer, Parent per) {
         super(nom, prenom, numeroTelephone, adresse, dateNaissance);
-		this.niveauScolaire = niveauScolaire;
-		this.id = id;
-		this.mer = mer;
-		this.per = per;
+        this.niveauScolaire = niveauScolaire;
+        this.id = id;
+        this.mer = mer;
+        this.per = per;
     }
+
+    @Override
+
+    public String toString() {
+
+        return super.toString() + "\nEleve [niveauScolaire=" + niveauScolaire + ", id=" + id + "\n mer=" + mer
+                + "\n per=" + per + "]";
+    }
+    
+    public Eleve() {
+        super();
+    }
+
+    public String getNiveauScolaire() {
+        return niveauScolaire;
+    }
+
+    public void setNiveauScolaire(String niveauScolaire) {
+		this.niveauScolaire = niveauScolaire;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Parent getMer() {
+		return mer;
+	}
+	public void setMer(Parent mer) {
+		this.mer = mer;
+	}
+	public Parent getPer() {
+		return per;
+	}
+
+    public void setPer(Parent per) {
+        this.per = per;
+    }
+    public void create(List<Eleve> list , Eleve d) {
+		list.add(d);
+	}
 
 }
