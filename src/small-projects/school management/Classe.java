95d3641758f4e0390645412a52679d41ebe6f54a
unknown <doniaskima344@gmail.com>
2022-11-07 12:57:48 +0100

    start creating the Classe class

--- src/small-projects/school management/Classe.java
+++ src/small-projects/school management/Classe.java
@@ -1,5 +1,13 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Classe {
-    
+    protected int id;
+    protected float fraisInscription;
+    protected String name;
+    protected ArrayList<Animateur> animateurs;
+    protected ArrayList<Eleve> eleve;
+    public class(){
+        super();
+    }
 }
