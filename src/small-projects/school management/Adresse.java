3e42c60936e5d65704928b2e6f488a2e147cab5a
unknown <doniaskima344@gmail.com>
2022-11-07 02:54:51 +0100

    workng on Adress class

---
+++ src/small-projects/school management/Adresse.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Adresse {
+    protected int numberOne;
+    protected String rue;
+    protected String ville;
+    protected int codePostal;
+    protected String gouvernorat;
+    protected int id;
+}
