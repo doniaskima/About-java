f28e2c494d420237e1157b4b70d06b1bcc171563
unknown <doniaskima344@gmail.com>
2022-11-07 03:02:53 +0100

    progress

---
+++ src/small-projects/school management/main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+
+        ArrayList<Eleve> listEleve = new ArrayList<Eleve>();
+        List<Parent> listParent = new ArrayList<Parent>();
+        List<Adresse> listeAdress = new ArrayList<Adresse>();
+        List<Classe> listClasse = new ArrayList<Classe>();
+        ArrayList<Eleve> lEleveList = new ArrayList<Eleve>();
+        
+
+    }
+}
