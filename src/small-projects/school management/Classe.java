3752d67edf9a2a03c6c43ee053ab538188178458
unknown <doniaskima344@gmail.com>
2022-11-07 13:02:46 +0100

    working on Classe class

--- src/small-projects/school management/Classe.java
+++ src/small-projects/school management/Classe.java
@@ -7,7 +7,26 @@ public class Classe {
     protected String name;
     protected ArrayList<Animateur> animateurs;
     protected ArrayList<Eleve> eleve;
-    public class(){
+
+    public Classe() {
         super();
     }
+
+    public Classe(int id, float fraisInscription, String nom, ArrayList<Animateur> animateurs, ArrayList<Eleve> eleve) {
+        super();
+        this.id = id;
+        this.fraisInscription = fraisInscription;
+        this.nom = nom;
+        this.animateurs = animateurs;
+        this.eleve = eleve;
+    }
+    @Override
+
+    public String toString() {
+        return "Classe " + nom + "\n [id=" + id + ",fraisInscription" + fraisInscription + "dinars"
+                + ",\n animateurs:\n" + animateurs + ", eleve=" + eleve + "]";
+    }
+    
+
+
 }
