8ef3df4dcc9660e46aa990fe5074ea7a248940a1
unknown <doniaskima344@gmail.com>
2022-11-07 12:46:50 +0100

    Animateur class

--- src/small-projects/school management/Animateur.java
+++ src/small-projects/school management/Animateur.java
@@ -1,3 +1,11 @@
-public class Animateur {
-    
+import jaca.util.Date;
+
+public class Animateur extends Person{
+    public Animateur() {
+        super();
+    }
+
+    public Animateur(String nom, String prenom, int numeroTelephone, Adresse adresse, java.sql.Date dateNaissance) {
+    super(nom,prenom,numeroTelephone,dateNaissance, adresse);
+   }
 }
