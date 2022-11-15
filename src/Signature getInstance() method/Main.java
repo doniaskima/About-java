bdd201beae47e3221162250e1121f1c4755a6802
unknown <doniaskima344@gmail.com>
2022-11-15 18:57:17 +0100

    add animateur

--- src/Signature getInstance() method/Main.java
+++ src/Signature getInstance() method/Main.java
@@ -4,7 +4,6 @@ import java.security.*;
 public class Main {
     try
     {
-        
         String algorithm = "SHA224withRSA"; // we define the Signature algorithm.
         Signature signature = Signature.getInstance(algorithm);
         // We obtain an instance of The Signature class using the getInstance() method by specifing the algorith defined in Line 9
@@ -12,5 +11,5 @@ public class Main {
 
     }catch(NoSuchAlgorithmException e){
         System.out.println("Exeption :" +e);// We catch the NoSuchAlgorithmException exception and print it.
-    }
+    } 
 }
--- src/small-projects/school management/main.java
+++ src/small-projects/school management/main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.text.SimpleDateFormat;
 
+
 public class main {
     public static void main(String[] args) {
 
@@ -19,7 +20,23 @@ public class main {
         /******************************add animateur************************************/
 
         simpleDateFormat sdf = new SimpleDateFormat("MM--dd-yyyy");
-        Calendar animateurdate 
+        Calendar animateur1date = Calendar.getInstance();
+        animateur1date.set(Calendar.YEAR, 2001);
+        animateur1date.set(Calendar.MONTH, 12 - 1);//months start at 0
+        animateur1date.set(Calendar.DAY_OF_MONTH, 20);
+        java.sql.Date dateAnimateur1 = new java.sql.Date(animateur1date.getTimeInMillis());
+        //return the current time of this Calendar in Milliseconds.
+        sdf.format(dateAnimateur1);
+        Animateur animateur1 = new Animateur("Marwa", "Majdoub", 12345252, null, dateAnimateur1);
+        Animateur animateur2 = new Animateur("Sirine", "Sarray", 12345252, null, dateAnimateur1);
+        Animateur animateur3 = new Animateur("Jiji", "Skima", 12345252, null, dateAnimateur1);
+        Animateur animateu4 = new Animateur("Islem", "Skima", 12345252, null, dateAnimateur1);
+        Animateur animateur1 = new Animateur("Hanin", "Dhaou", 12345252, null, dateAnimateur1);
+        listeAnimateur.add(animateur4);
+        listeAnimateur.add(animateur3);
+        listeAnimateur.add(animateur2);
+        listeAnimateur.add(animateur1)
+
 
     }
 }
