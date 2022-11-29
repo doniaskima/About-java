103616150bb8d7e6d75296157a3d46259fae6040
unknown <doniaskima344@gmail.com>
2022-11-29 03:20:48 +0100

    getOrder

--- src/small-projects/RestaurantManagement/Waiter.java
+++ src/small-projects/RestaurantManagement/Waiter.java
@@ -1,3 +1,46 @@
+import java.util.List;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Waiter {
-    
+    private static int number;
+    public static void getOrder() {
+        Scanner scanner = new Scanner(System.in);
+        try{
+            number=Integer.valueOf(scanner.nextInt());
+        } catch (InterruptedException e) {
+            System.out.println("Please enter numbers only .");
+            number = Integer.valueOf(scanner.nextInt());
+        }
+
+        while (number > 3) {
+            System.out.println("We only have 3 items Today . Please try again");
+            number = scanner.nextInt();
+        }
+
+        if (number == 1) {
+            Cook.setOption(number);
+        }else if (number==2){
+            Cook.setOption(number);
+        } else {
+            Cook.setOption(number);
+        }
+
+        while (number != 1 && number != 2 && number != 3) {
+            System.out.println("Please enter numbers only");
+            try{
+                number = Integer.valueOf(scanner.nextLine());
+                if(number>3){
+                    int x=1/0;
+                }
+            } catch (ArithmeticException e) {
+                System.out.println("We only have 3 items today. Please enter again.");
+				number = scanner.nextInt();
+            }catch (InputMismatchException e) {
+				System.out.println("Please enter numbers only.");
+				number = Integer.valueOf(scanner.nextLine());
+			}
+        }
+    }
 }
