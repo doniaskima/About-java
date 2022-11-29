97363b845cbe5a36aeb6a79fef2b1d10d353946a
unknown <doniaskima344@gmail.com>
2022-11-29 02:12:40 +0100

    Food class added

--- src/small-projects/RestaurantManagement/Food.java
+++ src/small-projects/RestaurantManagement/Food.java
@@ -1,3 +1,13 @@
 public class Food {
+    private String name;
+    private int price, quantity = 1;
+
+    public void setFoodDetail(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
     
+    public String toString(){
+        return "Name" + name + "Price :" + price;
+    }
 }
--- src/small-projects/RestaurantManagement/Restaurant.java
+++ src/small-projects/RestaurantManagement/Restaurant.java
@@ -3,15 +3,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Restaurant {
-    
+
     public static void displayWelcomeMessage() {
         System.out.println("---Welcome to our  restaurant :)");
         try {
             Thread.sleep(1000)
         } catch (InterruptedException e) {
-             e.printStackTrace();
+             e.printStackTrace(); //gerer les exeptions et les erreurs .
         }
 
         System.out.println("Please choose your table and enter table number")
     }
+    
 }
