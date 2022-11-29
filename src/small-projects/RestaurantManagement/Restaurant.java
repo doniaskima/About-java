fd5f460e8ce6695b0a67def0827e839df6683c81
unknown <doniaskima344@gmail.com>
2022-11-29 02:06:28 +0100

    1 welcome

--- src/small-projects/RestaurantManagement/Restaurant.java
+++ src/small-projects/RestaurantManagement/Restaurant.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Restaurant {
     
+    public static void displayWelcomeMessage() {
+        System.out.println("---Welcome to our  restaurant :)");
+        try {
+            Thread.sleep(1000)
+        } catch (InterruptedException e) {
+             e.printStackTrace();
+        }
+
+        System.out.println("Please choose your table and enter table number")
+    }
 }
