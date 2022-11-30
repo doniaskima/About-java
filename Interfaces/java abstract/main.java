aed81df5d056ea99fd44d4d0b8990d477a36c0dc
unknown <doniaskima344@gmail.com>
2022-11-30 23:15:07 +0100

    abstract

---
+++ Interfaces/java abstract/main.java
@@ -0,0 +1,28 @@
+// an abstract method belongs to an abstract class , and it does not have a body .
+//The body is provided by the subclass :
+
+abstract class Main {
+    public String fname = "John";
+    public int age = 24;
+    public abstract void study(); // abstract method
+} // abstract method
+
+//subclass (inherit from Main)
+
+class Student extends Main{
+    public int graduationYear=2018;
+    public void study(){
+        System.out.println("Studying all day long");
+    }
+}
+
+
+class Second{
+    public static void main(String[] args){
+        Student myObj = new Student();
+        System.out.println("Name: " + myObj.fname);
+        System.out.println("Age: " + myObj.age);
+        System.out.println("Graduation Year: " + myObj.graduationYear);
+        myObj.study();
+    }
+}
